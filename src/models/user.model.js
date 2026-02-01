6a6b51546e61c530e2e9da01fd11bf4d4437ea91
Bikashxkhanal <khanalbikash007@gmail.com>
2026-02-01 13:39:15 +0545

    Cloudinary service setup for storing photos and videos

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -57,12 +57,42 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      //payload for the token
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    //access token should be provided
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  ); //generate access token
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      //payload for the token
+      _id: this._id,
+    },
+    //access token should be provided
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
 };
 
 export const User = mongoose.model("User", userSchema);
---
+++ src/utils/Cloudinary.files.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+(async function cloudinary() {
+  // Configuration
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+  });
+})();
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const uploadResult = await cloudinary.uploader
+      .upload(localFilePath, {
+        resource_type: "auto",
+      })
+      .catch((error) => {
+        console.log(error);
+        throw error;
+      });
+
+    console.log("File Upload ", uploadResult);
+    return uploadResult.url;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // unlink the temporty file saved in local server when the upload fails
+    console.log(error);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
