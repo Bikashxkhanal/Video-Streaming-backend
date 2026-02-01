16fb02926641a48474ae37ec47e863abe43dae58
Bikashxkhanal <khanalbikash007@gmail.com>
2026-02-01 15:11:08 +0545

    Pre setup for backend with postman

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,17 +1,18 @@
-
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
-    }
-}
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
-const asyncHandler =  (requestHandler) => async (req, res, next) => {
-    try{
-        await requestHandler(req, res, next);
-    }catch(err){
-        res.status(err.code || 500).json({
-            success : false, 
-            message : err.message,
-        })
-    }
-}
+// const asyncHandler =  (requestHandler) => async (req, res, next) => {
+//     try{
+//         await requestHandler(req, res, next);
+//     }catch(err){
+//         res.status(err.code || 500).json({
+//             success : false,
+//             message : err.message,
+//         })
+//     }
+// }
+
+export { asyncHandler };
