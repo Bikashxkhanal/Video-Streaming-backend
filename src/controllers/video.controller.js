3f85587ad5fb5648b64bae0dd4c29780b8549914
Bikashxkhanal <khanalbikash007@gmail.com>
2026-02-03 14:01:48 +0545

    added getVideoByID endpoint to get a video by thier id

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -5,6 +5,7 @@ import { ApiResponse } from "../utils/ApiResponse";
 import { Video } from "../models/video.model.js";
 import { isNumberObject } from "util/types";
 import mongoose from "mongoose";
+import { pipeline } from "stream";
 
 const getAllVideos = asyncHandler(async (req, res) => {
   const {
@@ -24,7 +25,6 @@ const getAllVideos = asyncHandler(async (req, res) => {
     //first pipeLine to filter the vides if the userId is given(if userId is there filter videos and pass the videso of that uesr only based on that, if not pass all the videos to next stage)
     if (!mongoose.Types.ObjectId.isValid(userId)) {
       throw new ApiError(400, "Invalid userid");
-      return;
     }
     pipeline.push({
       $match: {
@@ -89,7 +89,58 @@ const getAllVideos = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, allVideos, "All videos are retrieved!"));
 });
 
-const getVideoById = asyncHandler(async (req, res) => {});
+const getVideoById = asyncHandler(async (req, res) => {
+  const { videoId } = req.query;
+
+  if (!videoId) {
+    throw new ApiError(400, "Video Id is needed");
+  }
+  if (videoId?.trim() === "") {
+    throw new ApiError(400, "Video Id is needed");
+  }
+
+  if (!mongoose.Types.ObjectId(videoId)) {
+    throw new ApiError(400, "Video Id is invalid");
+  }
+  const video = await video.pipeline([
+    //match the vides from the given Id
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+
+    //lookup for the owner of the video
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "videoOwner",
+        pipeline: [
+          {
+            $project: {
+              avatar: 1,
+              username: 1,
+              fullName: 1,
+              coverImage: 1,
+            },
+          },
+        ],
+      },
+    },
+
+    {
+      $addField: {
+        $first: "$videoOwner",
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video retrieved Successfully!"));
+});
 
 const publishAVideo = asyncHandler(async (req, res) => {});
 
